96c37b1d7f4b5f576150283c314520de3560f943
laure_sterne <laure.sterne@student.adatechschool.fr>
2022-02-13 16:59:56 +0100

    Begin level 2
    modify level 1 and start level 2

--- poker/index.js
+++ poker/index.js
@@ -1,46 +1,49 @@
-// program to shuffle the deck of cards
+/* Level 1 : modify and understand this program to shuffle the deck of cards
 
-// declare card elements
-const suits = ["Spades", "Diamonds", "Club", "Heart"];
-const values = [
-  "Ace",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "Jack",
-  "Queen",
-  "King",
-];
+declare card elements */
+const suits = ["♠︎ Spades", "♢ Diamonds", "♣︎ Club", "♡ Heart"];
+const values = [ "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+"Jack", "Queen", "King"];
 
 // empty array to contain cards
 let deck = [];
 
-// create a deck of cards
-for (let i = 0; i < suits.length; i++) {
-    for (let x = 0; x < values.length; x++) {
-        let card = { Value: values[x], Suit: suits[i] };
-        deck.push(card);
+function createDeck(couleurs, valeurs){ 
+    // create a deck of cards
+    for (let i = 0; i < couleurs.length; i++) {
+        for (let x = 0; x < valeurs.length; x++) {
+            let card = valeurs[x] + " " + couleurs[i];
+            deck.push(card);
+        }
     }
-}
 
-// shuffle the cards
-for (let i = deck.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * i);
-    let temp = deck[i];
-    deck[i] = deck[j];
-    deck[j] = temp;
-}
+    // shuffle the cards
+    for (let i = deck.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * i);
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+   return deck;
+};
 
-console.log('Your two cards are:');
+// console.log(createDeck(suits, values));
 
-// display 2 results
-for (let i = 0; i < 2; i++) {
-    console.log(`${deck[i].Value} of ${deck[i].Suit}`)
-}
+
+// Level 2 : distribution and remove cards which are in hand's players
+
+let hand = [];
+
+function deal(numberOfCards){
+    console.log('Your two cards are:');
+
+    // display 2 results
+    for (let i = 0; i < numberOfCards; i++) {   
+        result = Math.floor(Math.random * deck[i])
+        hand.push(result);
+    }
+    return hand;
+};
+
+console.log(deal(3));
 
