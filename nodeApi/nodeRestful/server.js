163600ff9e53878d844671dbc28e56ba5bcfe1b7
laure_sterne <laure.sterne@student.adatechschool.fr>
2022-03-05 12:18:31 +0100

    get request to obtain a specific user

--- nodeApi/nodeRestful/server.js
+++ nodeApi/nodeRestful/server.js
@@ -9,7 +9,18 @@ app.get('/listUsers', function (req, res) {
    });
 })
 
-var server = app.listen(8081, function () {
+
+app.get('/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       var users = JSON.parse( data );
+       var user = users["user" + req.params.id] 
+       console.log( user );
+       res.end( JSON.stringify(user));
+    });
+})
+
+var server = app.listen(8080, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
