0ed39936db29ef99080cf6cd6522c914ec4ab338
laure_sterne <laure.sterne@student.adatechschool.fr>
2022-03-05 16:53:19 +0100

    condition of victory (in line) and next player

--- puissanceQuatre/puissanceQuatreObject.py
+++ puissanceQuatre/puissanceQuatreObject.py
@@ -30,14 +30,29 @@ class Game:
         print(self.grid)
     
     def endOfTheGame(self): #boolean
-        pass
-                    
-    # def nextPlayer(self):
-    #     if self.player == 1:
-    #         self.player += 1
-    #     else:
-    #         self.player -= 1
-    #     pass
+        win = 0
+
+        for line in self.grid:
+            # print(line)
+            for column in line:
+                # print(column)
+                if column == self.player:
+                    win += 1
+                    print("count win: ", win)
+                else:
+                    win = column
+                    # print("reset count win:", win)
+        
+        if win >= 4:
+            return True
+        else:
+            return False
+
+    def nextPlayer(self):
+        if self.player == 1:
+            self.player += 1
+        else:
+            self.player -= 1
 
 
 # def playGameRound(self):
@@ -51,7 +66,10 @@ class Game:
 structure = Game()
 # structure.playGameRound()
 
-structure.show()
-structure.placeAPawn()
-structure.endOfTheGame()
-
+# structure.show()
+# structure.placeAPawn()
+# structure.nextPlayer()
+# structure.placeAPawn()
+# structure.show()
+# structure.endOfTheGame()
+# print(structure.endOfTheGame())
