2e7b9eba77bbb8c2bc104dc2b9ad5732b1874dc4
laure_sterne <laure.sterne@student.adatechschool.fr>
2022-03-23 11:54:28 +0100

    beginning of this exercise

---
+++ multithreading/multithreading.py
@@ -0,0 +1,15 @@
+import os
+import logging
+
+class CompressOneImage:
+    pass
+
+class ProcessImages:
+
+    # filenames will be a list of all files found in directory
+    for (dirpath, dirnames, filenames) in os.walk(multithreading.bestImages):
+        print(dirpath)
+        print(dirnames)
+        print(filenames)
+
+logging.basicConfig(format="%(threadName)s:%(message)s")
