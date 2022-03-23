86323f0bce6491b0e1b52f7487f37181397b7fa5
laure_sterne <laure.sterne@student.adatechschool.fr>
2022-03-23 11:55:40 +0100

    clean and reorganization

--- puissanceQuatre/puissanceQuatreObject.py
+++ puissanceQuatre/puissanceQuatreObject.py
@@ -1,6 +1,6 @@
 # Step 1 : Structure
 class Game:
-    print("Mise en place de la grille")
+    print("Start of the game!")
 
     def __init__(self, nblines = 6, nbcolumns = 7):
         self.player = 1
@@ -16,34 +16,34 @@ class Game:
     
     # Step 2 : Show the grid of the game  
     def show(self):
-        print(self.grid)
+        for i in range(self.nbcolumns - 1):
+            print(self.grid[-i-1])
+        print("Round of player", self.player)
 
-    def placeAPawn(self, column = input("Choose your column to place your pawn: ")):
+    def placeAPawn(self, column):
         self.column = int(column)
 
         for i in self.grid:
             if i[self.column] == 0:
                 i[self.column] = self.player
-                print("I place my pawn with my number's player", i[self.column], "below")
                 break
-
-        print(self.grid)
     
     def endOfTheGame(self): #boolean
         win = 0
-
+        
         for line in self.grid:
             # print(line)
             for column in line:
                 # print(column)
                 if column == self.player:
                     win += 1
-                    print("count win: ", win)
+                    print(win)
                 else:
                     win = column
                     # print("reset count win:", win)
         
         if win >= 4:
+            print("You are the winner of this game:", self.player)
             return True
         else:
             return False
@@ -57,9 +57,9 @@ class Game:
 
     def playGameRound(self):
         self.show()
-        self.placeAPawn()
-    
-        if not self.endOfTheGame():
+        self.placeAPawn(column = input("Choose your column to place your pawn: "))
+
+        if self.endOfTheGame() == False:
             self.nextPlayer()
             self.playGameRound()
 
