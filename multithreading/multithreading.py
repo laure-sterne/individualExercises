6cee81a77b15446b8c31d358cd7d007338225cab
ahau7 <laure.sterne@student.adatechschool.fr>
2022-04-03 13:11:08 +0200

    see contents of images folder + keep only jpg images

--- multithreading/multithreading.py
+++ multithreading/multithreading.py
@@ -1,15 +1,27 @@
 import os
+from os import walk
 import logging
 
+# from PIL import Image
 class CompressOneImage:
     pass
 
+# class who scan the folder and start a thread by image to compress
 class ProcessImages:
 
-    # filenames will be a list of all files found in directory
-    for (dirpath, dirnames, filenames) in os.walk(multithreading.bestImages):
-        print(dirpath)
-        print(dirnames)
-        print(filenames)
+    def contentsFolder(self):
+
+        for (dirpath, dirnames, filenames) in walk("./bestImages"):
+            print("I am the path to the folder:", dirpath)
+            print("I am the folders inside this folder:", dirnames)
+            print("I am the files inside this folder:", filenames)
+    
+        for i in filenames:          
+            fileName, fileExtension = os.path.splitext(i)
+        
+        images = [i for i in filenames if fileExtension == ".jpg"]
+        print("images jpg only:", images)        
 
 logging.basicConfig(format="%(threadName)s:%(message)s")
+
+ProcessImages().contentsFolder()
