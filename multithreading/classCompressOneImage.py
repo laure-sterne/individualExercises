11d779032ba205919d225e6f21528b19aa58570c
ahau7 <laure.sterne@student.adatechschool.fr>
2022-04-05 11:20:12 +0200

    structure of compressing and save  an image

--- multithreading/classCompressOneImage.py
+++ multithreading/classCompressOneImage.py
@@ -1,8 +1,31 @@
 import os
 import logging
+from pickletools import optimize
+from PIL import Image
 
 class CompressOneImage:
 
-    
-    
+    def __init__(self):
+
+        compressedImagefilepath = "./bestImages/compressedImages"
+        if not os.path.exists(compressedImagefilepath):
+            print("The folder doesn't exists, I will create it !")
+            os.makedirs(compressedImagefilepath)
+        else:
+            print("It's okay! The directory already exists!")
+
+        imageFilepath = "./bestImages/bebou.jpg"
+        picture = Image.open(imageFilepath)
+        print("I am opening the image")
+
+        imageFilesize = os.stat(imageFilepath).st_size
+        print("I am get size of this image:", imageFilesize, "bytes")
+
+        picture.save("compressed.jpg", "JPEG", optimize=True, quality=30)
+        print("I am compressing the image selected")
+
+        
+
     logging.basicConfig(format="%(threadName)s:%(message)s")
+
+CompressOneImage()
